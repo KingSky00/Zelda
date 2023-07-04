12055029914b307ed29ffbe0cdf446e352ff0d05
Malev <hniki123@abv.bg>
2023-07-04 14:24:42 +0300

    Create player and rock classes, implement map position logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys   # importing pygame library
 from settings import * # importing everything from settings file
+from level import Level
 
 class Game:
     def __init__(self):
@@ -7,7 +8,10 @@ class Game:
         # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT)) # initiating pygame / creating a display surface and clock
+        pygame.display.set_caption('Zelda')
         self.clock = pygame.time.Clock()
+
+        self.level = Level() # creating an instance of level class
 
     def run(self):
         while True:
@@ -17,7 +21,8 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
-            pygame.display.update() # updating the screen, color black and setting fps 
+            self.level.run()
+            pygame.display.update() # updating the screen, color black and setting fps
             self.clock.tick(FPS)
 
 if __name__ == '__main__':  # first check if this the main file
